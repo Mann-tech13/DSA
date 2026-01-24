36da9261d49376b3de3b592a5687727ee2d9abcf
Mann-tech13 <mannshah.ict19@gmail.com>
2026-01-24 16:37:16 +0530

    feat: 24-Jan-2026

--- Stack/930-sum-of-subarray-minimums-lc-medium.java
+++ Stack/930-sum-of-subarray-minimums-lc-medium.java
@@ -1,45 +1,41 @@
-// https://leetcode.com/problems/sum-of-subarray-minimums/
 class Solution {
+    public int[] nextSmallElementIndex(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[arr.length];
+        for(int i = arr.length - 1; i >= 0; i--) {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+            if(st.isEmpty()) res[i] = arr.length;
+            else res[i] = st.peek();
+            st.push(i);
+        }
+        return res;
+    }
+    public int[] prevSmallElementIndex(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]) st.pop();
+            if(st.isEmpty()) res[i] = -1;
+            else res[i] = st.peek();
+            st.push(i);
+        }
+        return res;
+    }
     public int sumSubarrayMins(int[] A) {
         int n = A.length;
-        int MOD = 1_000_000_007;
+        int mod = 1_000_000_007;
 
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int[] nseIndex = nextSmallElementIndex(A);
+        int[] pseIndex = prevSmallElementIndex(A);
+        long total = 0;
 
-        for (int i = 0; i < n; i++) {
-            left[i] = i + 1;
-            right[i] = n - i;
+        for(int i = 0; i < A.length; i++) {
+            long left = i - pseIndex[i];
+            long right = nseIndex[i] - i;
+
+            total = (total + left * right * A[i]) % mod;
         }
 
-        // Stacks storing {value, index}
-        Deque<int[]> prevLess = new ArrayDeque<>();
-        Deque<int[]> nextLess = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-
-            // Previous Less Element
-            while (!prevLess.isEmpty() && prevLess.peek()[0] > A[i]) {
-                prevLess.pop();
-            }
-            left[i] = prevLess.isEmpty() ? i + 1 : i - prevLess.peek()[1];
-            prevLess.push(new int[]{A[i], i});
-
-            // Next Less Element
-            while (!nextLess.isEmpty() && nextLess.peek()[0] > A[i]) {
-                int[] top = nextLess.pop();
-                right[top[1]] = i - top[1];
-            }
-            nextLess.push(new int[]{A[i], i});
-        }
-
-        // Calculate result
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            long contribution = (long) A[i] * left[i] * right[i];
-            ans = (ans + contribution) % MOD;
-        }
-
-        return (int) ans;
+        return (int)total;
     }
 }

aba44b676eac108d3022b2bf9988a811141e226b
Mann-tech13 <mannshah.ict19@gmail.com>
2026-03-14 21:32:56 +0530

    feat: 14 March 2026

---
+++ Neetcode150/Arrays and Hashing/valid-palindrome.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++; 
+            r--;
+        }
+        return true;
+    }
+
+    public boolean alphaNum(char ch) {
+        return (ch >= 'A' && ch <= 'Z' ||
+                ch >= 'a' && ch <= 'z' ||
+                ch >= '0' && ch <= '9');
+    }
+}
